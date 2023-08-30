f46406a029b7e8ff860d881a09a540e7fc38172e
caelan-g <141709327+caelan-g@users.noreply.github.com>
2023-08-31 07:43:29 +1000

    Updated word-count location, in progress

--- scripts/background.js
+++ scripts/background.js
@@ -1,5 +1,7 @@
 // console.log("working");
 let selected;
+let selectedX;
+let selectedY;
 let wordDisplay = document.createElement("word-display");
 let wordDisplayContent = document.createTextNode("Wordcount is");
 let wordDisplayNum = document.createElement("p");
@@ -27,6 +29,20 @@ function getSelectedText() {
   return selected;
 }
 
+function getSelectedXY() {
+  let selectedRect = selected.getBoundingClientRect();
+
+  //get position in related to viewport
+  selectedX = selectedRect.x;
+  selectedY = selectedRect.y;
+
+  //get position in relation to webpage
+  selectedX = selectedX + window.scrollX;
+  selectedY = selectedY + window.scrollY;
+
+  return selectedX, selectedY;
+}
+
 //mouse check loop that scans on mouse move
 document.addEventListener("mousemove", function (event) {
   mouseX = parseInt(event.clientX);
@@ -38,12 +54,16 @@ setInterval(function () {
   getSelectedText();
   length = selected.length;
   if (length > 0) {
+    getSelectedXY();
     //console.log("mouseX and Y is " + mouseX + " " + mouseY);
     wordCount = selected.split(" ").length;
     //console.log("Wordcount is " + wordCount);
-    wordDisplay.style.visibility = "visible";
+    /*wordDisplay.style.visibility = "visible";
     wordDisplay.style.top = mouseY + 10 + "px";
-    wordDisplay.style.left = mouseX + "px";
+    wordDisplay.style.left = mouseX + "px";*/
+
+    wordDisplay.style.top = selectedY;
+    wordDisplay.style.left = selectedX;
 
     wordDisplayNum.textContent = wordCount;
   } else {

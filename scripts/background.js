b05d77cee0af23f4b0fcd0f5041c94ade5c7457a
caelan-g <141709327+caelan-g@users.noreply.github.com>
2023-09-01 18:55:03 +1000

    Updated front-end

--- scripts/background.js
+++ scripts/background.js
@@ -5,30 +5,44 @@ let selectedY;
 let selectedString;
 let selectedRect;
 
-let wordDisplayNum = document.createElement("word-display");
+let wordDisplayText = document.createElement("word-display-text");
+let wordDisplayTextContent = document.createTextNode(" words.");
+
+let wordDisplayNum = document.createElement("word-display-num");
 let wordDisplayNumContent = document.createTextNode("0");
 
 let wordDisplay = document.createElement("word-display");
-let wordDisplayContent = document.createTextNode("Wordcount is");
 
-let link = document.createElement("link");
+/*let link = document.createElement("link");
 link.rel = "stylesheet";
 link.type = "text/css";
-link.href = "stylesheet.css";
+link.href = "/stylesheet.css";
+head.appendChild(link);*/
 
-wordDisplay.appendChild(wordDisplayContent);
+var link = document.createElement("link");
+link.setAttribute("rel", "stylesheet");
+link.setAttribute("type", "text/css");
+link.setAttribute(
+  "href",
+  "https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;700&display=swap"
+);
+document.head.appendChild(link);
+
+wordDisplayText.appendChild(wordDisplayTextContent);
 wordDisplayNum.appendChild(wordDisplayNumContent);
 
 let html = document.querySelector("html");
 let head = document.querySelector("head");
 let mouseX;
 let mouseY;
+
 html.appendChild(wordDisplay);
 wordDisplay.appendChild(wordDisplayNum);
+wordDisplay.appendChild(wordDisplayText);
 
 //wordDisplay.style.top = "0px";
 wordDisplay.style.position = "absolute";
-wordDisplay.style.backgroundColor = "green";
+//wordDisplay.style.backgroundColor = "green";
 wordDisplay.style.display = "inline-block";
 
 //function to get selected text in string
@@ -64,11 +78,28 @@ document.addEventListener("mousemove", function (event) {
   mouseY = parseInt(event.clientY);
 });
 
+function assignStyles() {
+  wordDisplay.style.backgroundColor = "rgb(240,240,240)";
+  wordDisplay.style.paddingLeft = "5px";
+  wordDisplay.style.paddingRight = "5px";
+  wordDisplay.style.paddingTop = "3px";
+  wordDisplay.style.paddingBottom = "3px";
+  wordDisplay.style.boxShadow = "2px 2px 3px rgba(0,0,0,.5";
+  wordDisplay.style.borderRadius = "5px";
+  wordDisplay.style.fontSize = "13px";
+  wordDisplay.style.fontWeight = "600";
+  wordDisplay.style.fontFamily = "Outfit, Arial, Sans-Serif";
+  wordDisplay.style.color = "rgb(10,10,10)";
+  wordDisplay.style.transition = "0.1s all ease-in-out";
+}
+
 //main scan loop
+assignStyles();
+
 setInterval(function () {
   getSelectedText();
   length = selectedString.length;
-  if (length > 1) {
+  if (length > 0) {
     getSelectedXY();
     //console.log("mouseX and Y is " + mouseX + " " + mouseY);
     wordCount = selectedString.split(" ").length;
@@ -79,12 +110,12 @@ setInterval(function () {
 
     wordDisplay.style.top = selectedY + 20 + "px";
     wordDisplay.style.left = selectedX + "px";
-    wordDisplay.style.visibility = "visible";
+    wordDisplay.style.opacity = "1";
 
     //console.log("X and Y are " + selectedX + ", " + selectedY);
 
     wordDisplayNum.textContent = wordCount;
   } else {
-    wordDisplay.style.visibility = "hidden";
+    wordDisplay.style.opacity = "0";
   }
 }, 10);
